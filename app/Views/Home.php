7e36fb3da3ef451931ef294b27608c4a5a0607aa
arricodyanto <arricohandyanto@gmail.com>
2021-05-18 02:02:17 +0700

    upload cobalagi

---
+++ app/Views/Home.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="shortcut icon" href="favicon.png" />
+    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <title>Laman Utama - Eleven March Project</title>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <!-- Home -->
+
+    <nav class="navbar navbar-expand-sm navbar-dark sticky-sm-top" style="background-color: #000000;" aria-label="Main navigation">
+        <div class="container-fluid cover-container">
+            <a class="navbar-brand" href="#home">
+                <img src="img/logo-white-c.png" style="width:80px">
+            </a>
+            <button class="navbar-toggler" type="button" id="navbarSideCollapse" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="navbar-collapse offcanvas-collapse" id="navbarprimary">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 p-2">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- <nav class="nav nav-masthead justify-content-center float-md-end">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">About</a>
+        <a class="nav-link" href="#">Contact</a>
+    </nav> -->
+    <section id="home">
+        <div class="bg-fit min-vh-100" style="background-image: url('img/home-darken.png');" onContextMenu='return false'>
+
+            <div class="cover-container d-flex w-100 h-100 p-3 mx-auto my-auto flex-column">
+                <div class="container mt-4">
+                    <div class="row">
+                        <div class="col">
+                            <!-- <div class="positition-relative"> -->
+                            <img src="img/logo.png" class="w-25" onContextMenu='return false'>
+                            <!-- </div> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    </div>
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <footer class="mt-auto text-white-50">
+            <p>Cover template for <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
+        </footer>
+    </div>
+
+    <script src="assets/bootstrap/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    <script src="js/add.min.js"></script>
+</body>
+
+</html>
